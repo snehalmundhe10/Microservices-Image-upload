dc578ce5e997f0bf82315b7f8ea1472854f530f5
Snehal Mundhe <m.snehal106@gmail.com>
2020-12-13 06:03:41 -0500

    feat: added route

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const uploadRoutes = require("./routes/api/uploads");
 
+app.use("*", cors());
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-  res.status(200).send({ success: true })
-});
+app.use(uploadRoutes);
 
 app.listen(4000);
