cb20ebdf46059d3ab165f5413601168d01ce00bc
Snehal Mundhe <m.snehal106@gmail.com>
2020-12-15 17:27:49 -0500

    fix:set_env.sh

---
+++ backend-image-api/config/config.js
@@ -0,0 +1,10 @@
+  const config = {
+  "development": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres"
+  }
+}
+module.exports = config;
